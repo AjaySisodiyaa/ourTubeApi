c697fda661dd0a34089ab5ecc5078190069b6f95
AjaySisodiyaa <ajay24sisodiya@gmail.com>
2025-08-22 19:22:31 +0530

    hearder added

--- routes/video.js
+++ routes/video.js
@@ -18,7 +18,15 @@ cloudinary.config({
 // get video url ------------------------------
 Router.post("/get-video-url", async (req, res) => {
   try {
-    const { data } = await axios.get(req.body.videoUrl);
+    const { data } = await axios.get(req.body.videoUrl, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36",
+        Accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+      },
+    });
     const $ = load(data);
     const player = $("#player");
     const videoUrl = player.length ? player.html().slice(94, 121) : null;
